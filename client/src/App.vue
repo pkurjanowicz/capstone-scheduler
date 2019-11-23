1d4b8a7b39c2bd63c7b8c191daef5651487e0f91
pkurjanowicz5 <pkurjanowicz5@gmail.com>
2019-11-22 19:38:03 -0700

    finished calendar view component

--- client/src/App.vue
+++ client/src/App.vue
@@ -14,7 +14,7 @@
     <div v-if="this.timeCheckbox == true">
       <button @click="updateZones" v-on:keyup.enter="updateZones">Update timezones</button>
       <select v-model="chosenTimezoneNumber" @click="selectTimezone">
-        <option v-for="(zone, key) in zones" :value="key">
+        <option v-for="(zone, key) in zones" :key="key">
           {{ zone }}
         </option>
       </select>
@@ -42,23 +42,16 @@
     <h2>My events</h2>
     <button @click="getEvents">Update my events</button>
     <br>
-    <div id=eventList>
-      <ul v-for="(event, index) in zippedEvent" :key="index">
-        <li> {{ event[0] }} </li>
-        <li> {{ event[1] }} </li>
-        <li> {{ event[2] }} </li>
-        <li> {{ event[3] }} </li>
-        <button @click="deleteEvent(event[4])">Delete event</button>
-        <p>---------------------------------</p>
-      </ul>
-    </div>
-    
+    <calendarView
+    :calendarEvents='zippedEvent'
+    />
   </div>
 </template>
 
 <script>
 import DatePicker from 'vue2-datepicker'
 import axios from 'axios'
+import calendarView from '/Users/peterkurjanowicz/Desktop/Interesting Projects/bronsons_project/capstone-scheduler/client/src/components/calendarView.vue'
 
 let moment = require('moment')
 
@@ -97,7 +90,8 @@ export default {
     }        
   },
   components: {
-    DatePicker
+    DatePicker,
+    calendarView
   },
   methods: {
     submitNewEvent() {
@@ -124,14 +118,13 @@ export default {
 
       axios.post('/newevent', { owner_id: this.currentUserID, event_name: this.eventName, event_details: this.eventDetails, event_start_time: this.startTime, event_end_time: this.endTime})
       .then(() => {
-        
-      })
         this.eventName = ''
         this.eventDetails = ''
         this.startTime = ''
         this.range = ''
         this.endTime = ''
         this.getEvents()
+      })
     },
     deleteEvent(id) {
       // There is currently an issue with the delete button failing to remove list items sometimes.
@@ -154,10 +147,6 @@ export default {
     },
     clearEventList() {
       this.zippedEvent = []
-      const listID = document.getElementById("eventList")
-      if (listID.firstChild) {
-        while (listID.firstChild) listID.removeChild(listID.firstChild)
-      }
     },
     getCurrentUserID() {
       axios.get('/user')
@@ -201,20 +190,29 @@ export default {
       axios.get('/getevents')
       .then((response) => {
         let currentResponse = response.data.all_events
+        console.log(currentResponse)
         for (let i = 0; i < currentResponse.length; i++) {
           if (this.currentUserID === currentResponse[i].owner_id) {
 
-            this.eventResponseNames.push(currentResponse[i].event_name)
-            this.eventResponseDetails.push(currentResponse[i].details)
-            this.eventResponseStartTime.push(currentResponse[i].start_time)
-            this.eventResponseEndTime.push(currentResponse[i].end_time)
-            this.eventResponseID.push(currentResponse[i].id)
+            this.eventResponseNames.push(currentResponse[i])
+            this.eventResponseDetails.push(currentResponse[i])
+            this.eventResponseStartTime.push(currentResponse[i])
+            this.eventResponseEndTime.push(currentResponse[i])
+            this.eventResponseID.push(currentResponse[i])
           }
         }
         for (let i = 0; i < this.eventResponseNames.length; i++) {
-          let stringConvertStartTime = this.eventResponseStartTime[i].toString()
-          let stringConvertEndTime = this.eventResponseEndTime[i].toString()
-          this.zippedEvent.push([stringConvertStartTime, stringConvertEndTime, this.eventResponseNames[i], this.eventResponseDetails[i], this.eventResponseID[i]])
+          let stringConvertStartTime = this.eventResponseStartTime[i].start_time.toString()
+          let stringConvertEndTime = this.eventResponseEndTime[i].end_time.toString()
+          //the zipped event must be in this format in order for calendarview to display it(as an object) **PK
+          this.zippedEvent.push({
+            title: this.eventResponseNames[i].event_name, 
+            start: stringConvertStartTime, 
+            end: stringConvertEndTime, 
+            })
+            // details: this.eventResponseDetails[i].details, ** PK
+            // id: this.eventResponseID[i].id ** PK
+            //Maybe I will use these later? ** PK
         }
         this.eventResponseNames = []
         this.eventResponseDetails = []
@@ -247,7 +245,7 @@ export default {
     color: red;
   }
   #app{
-    background-image: url("assets/photo-1455612693675-112974d4880b.jpeg");
+    /* background-image: url("assets/photo-1455612693675-112974d4880b.jpeg"); */
     background-size: 200px;
   }
   textarea {
